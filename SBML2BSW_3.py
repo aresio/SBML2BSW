9b22977b4b4a8b9427a76a9f2dde125f29648115
Ilario Tagliaferri <ilario.tagliaferri@gmail.com>
2017-07-25 17:07:03 +0200

    Traduced the script for python3 (already tested) It should be better for the iterface implementation

--- SBML2BSW_3.py
+++ SBML2BSW_3.py
@@ -35,9 +35,9 @@ def separator():
     """
     Prints a separator fo the output
     """
-    print
-    print "*"*100
-    print
+    print ()
+    print ("*"*100)
+    print ()
 
 
 class SBMLloader(object):
@@ -48,26 +48,26 @@ class SBMLloader(object):
         self.ISDET = False
         if file_name!="":
             self.reader     = libsbml.SBMLReader()
-            self.sbml       = self.reader.readSBML(file_name)    
-            self.model = self.sbml.getModel()            
-            if self.model==None: 
-                print "ERROR, empty model or file not valid:", file_name
+            self.sbml       = self.reader.readSBML(file_name)
+            self.model = self.sbml.getModel()
+            if self.model==None:
+                print ("ERROR, empty model or file not valid:", file_name)
                 sys.exit(-2)
             else:
-                print " * File", file_name,"loaded correctly"
+                print (" * File", file_name,"loaded correctly")
         else:
-            print " * Please specify a SBML file"
+            print (" * Please specify a SBML file")
 
 
     def convert_to_biosimware(self, OUTPATH, verbose=False):
 
-        """        
+        """
         This method creates an output folder calling
         self.create_folder(self,OUTPATH), processes the SBML with
         self.process_sbml(self) and save the results in file in OUTPATH
-        
-        """     
-        
+
+        """
+
         self.create_folder(OUTPATH)
         self.process_sbml()
 
@@ -77,30 +77,30 @@ class SBMLloader(object):
         cwd = os.getcwd()
         os.chdir(OUTPATH)
 
-        if verbose: print " * Creating 'M_0' with initial state...",
+        if verbose: print (" * Creating 'M_0' with initial state..."),
         savetxt("M_0", [self.initial_amounts], fmt="%e", delimiter="\t")
-        if verbose: print "DONE"
-        
-        if verbose: print " * Creating 'M_feed' file...",        
+        if verbose: print ("DONE")
+
+        if verbose: print (" * Creating 'M_feed' file..."),
         for n,species in enumerate(self.model.getListOfSpecies()):
             if species.constant: feeds[n]=1
         numpy.savetxt("M_feed", [feeds], fmt="%d", delimiter="\t")
-        if verbose: print "DONE"
+        if verbose: print ("DONE")
 
-        if verbose: print " * Creating 'alphabet' with names of species...", 
+        if verbose: print (" * Creating 'alphabet' with names of species..."),
         with open("alphabet", "w") as fo:
             for (k,v) in self.sorted_species_dict: fo.write(k+"\t")
-        if verbose: print "DONE"
-        
-        if verbose: print " * Creating 'left_side' matrix with reactants",
+        if verbose: print ("DONE")
+
+        if verbose: print (" * Creating 'left_side' matrix with reactants"),
         numpy.savetxt("left_side", self.LEFT, fmt="%d", delimiter="\t")
-        if verbose: print "DONE"; print " * Creating 'right_side' matrix with products...",
+        if verbose: print ("DONE"); print (" * Creating 'right_side' matrix with products..."),
         numpy.savetxt("right_side", self.RIGHT, fmt="%d", delimiter="\t")
-        if verbose: print "DONE"; print " * Creating 'c_vector' file for parameters...",
+        if verbose: print ("DONE"); print (" * Creating 'c_vector' file for parameters..."),
         numpy.savetxt("c_vector", numpy.array([self.PARAMS]), fmt="%e", delimiter="\n")
-        if verbose: print " * Creating 'boundaries' matrix for FBA fluxes limits...", 
+        if verbose: print (" * Creating 'boundaries' matrix for FBA fluxes limits..."),
         numpy.savetxt("boundaries", self.fluxes_boundaries, fmt="%e", delimiter="\t")
-        if verbose: print "DONE"
+        if verbose: print ("DONE")
 
         os.chdir(cwd)
 
@@ -109,9 +109,9 @@ class SBMLloader(object):
         Creates the specified output folder
         """
         try: os.mkdir(OUTPATH)
-        except: print "WARNING: directory", OUTPATH, "already exists"
+        except: print ("WARNING: directory", OUTPATH, "already exists")
 
-        
+
     def process_sbml(self):
         """
         Creates the model object,creates a dictionary of species ID , gets
@@ -123,7 +123,7 @@ class SBMLloader(object):
         """
         # create dictionary of species IDs
         self.id2name = {}
-        for species in self.model.getListOfSpecies():            
+        for species in self.model.getListOfSpecies():
             fullname = species.getName()
             if fullname == "":
                 fullname = species.getId()
@@ -133,29 +133,29 @@ class SBMLloader(object):
             self.id2name[species.getId()] = fullname
 
         # create reverse dictionary of species indices
-        self.species_dict = {}        
-        
+        self.species_dict = {}
+
         # container of initial species amounts (sorted)
         self.initial_amounts = self.get_initial_amounts()
         tots_species = len(self.initial_amounts)
-        
+
         self.sorted_species_dict = sorted(self.species_dict.items(),  key=operator.itemgetter(1))
-       
+
         self.LEFT, self.RIGHT, self.PARAMS = [], [], []
-        self.get_reactions()    
+        self.get_reactions()
 
 
-    def get_initial_amounts(self, verbose=True):        
+    def get_initial_amounts(self, verbose=True):
         """Detects species in each comparment, gets the amount and the units
         of each specie, fills the species dictionary.
 
         """
         ini = []
 
-        print " * Detected", len(self.model.getListOfSpecies()), "species"
+        print (" * Detected", len(self.model.getListOfSpecies()), "species")
 
         n = 0
-        for species in self.model.getListOfSpecies(): 
+        for species in self.model.getListOfSpecies():
 
             compartment = species.getCompartment()
 
@@ -163,15 +163,15 @@ class SBMLloader(object):
                 if species.getSubstanceUnits()=="mole":
                     self.ISDET=True
 
-        
+
             name = ""
             if species.getName()!="": name = species.getName()
-            else:                     name = species.getId()            
-            if compartment!="": 
+            else:                     name = species.getId()
+            if compartment!="":
                 name = name + "_in_"+compartment
 
             if verbose:
-                print " * Parsing species", n, name
+                print (" * Parsing species", n, name)
 
             amount = 0
             if isnan(species.getInitialAmount()):
@@ -182,16 +182,16 @@ class SBMLloader(object):
             # print "Initial concentration:", species.getInitialConcentration()
             # print "Initial amount:", species.getInitialAmount()
 
-            try:            
+            try:
                 if int(amount)==amount:
                     amount = int(amount)
             except:
-                print "ERROR: cannot test amount of species", name
+                print ("ERROR: cannot test amount of species", name)
                 exit(1)
-            
-            
+
+
             try:
-                self.species_dict[name]                            
+                self.species_dict[name]
             except:
                 self.species_dict[name]=n
                 ini.append(amount)
@@ -222,21 +222,21 @@ class SBMLloader(object):
         if use_fba:
             # FBA stuff
             fbc = self.model.getPlugin("fbc")
-            print " * Detected objectives:", fbc.getNumObjectives()
+            print (" * Detected objectives:", fbc.getNumObjectives())
             for o in xrange(fbc.getNumObjectives()):
-                OF = ObjectiveFunction()            
+                OF = ObjectiveFunction()
                 SBOf = fbc.getObjective(o)
                 OF.name = SBOf.getName()
                 OF.ID = SBOf.getId()
                 OF.type = SBOf.getType()
-                print " * Objective function detected:", OF.type, OF.ID
-                
+                print (" * Objective function detected:", OF.type, OF.ID)
+
                 # for ofl_ in range(SBOf.getNumFluxObjectives()):
                 #    print ofl_
                 for fo in xrange(SBOf.getNumFluxObjectives()):
                     fluxobj = SBOf.getFluxObjective(fo)
-                    print " * Coefficient", fo, "for reaction", fluxobj.getReaction(), "is equal to", fluxobj.getCoefficient()
-                
+                    print (" * Coefficient", fo, "for reaction", fluxobj.getReaction(), "is equal to", fluxobj.getCoefficient())
+
 
         # get dictionary of parameters
         PARAM_D = {}
@@ -263,7 +263,7 @@ class SBMLloader(object):
             ufbid = RFBCplg.getUpperFluxBound()
             #print lfbid, ufbid, PARAM_D[lfbid]['value'], PARAM_D[ufbid]['value']
         """
-        
+
         for react in self.model.getListOfReactions():
 
             reactants_vector = numpy.zeros(tots_species)
@@ -281,39 +281,39 @@ class SBMLloader(object):
                 self.fluxes_boundaries.append( [PARAM_D[lfbid]['value'], PARAM_D[ufbid]['value']] )
 
             if react.getKineticLaw() != None:
-            
+
                 # gestire piu di un parametro!
                 if len(react.getKineticLaw().getListOfParameters())==0:
                     #print "WARNING: cannot find local kinetic parameters"
-                    nomi_parametri = [name.getId() for name in self.model.getListOfParameters() ]
+                    nomi_parametri = [name.getId() for name in self.model.getListOfParameters()]
                     parameters_in_kineticaw = re.findall(r"[\w']+", react.getKineticLaw().getFormula())
                     parameters_in_kineticaw = [x.strip() for x in parameters_in_kineticaw ]
-                    parameters_in_kineticaw = filter( lambda x: x in nomi_parametri, parameters_in_kineticaw )
-                     #print react.getName(), "PIKL:", parameters_in_kineticaw
+                    parameters_in_kineticaw = list(filter( lambda x: x in nomi_parametri, parameters_in_kineticaw ))
+                    print (react.getName(), "PIKL:", parameters_in_kineticaw)
                     if len(parameters_in_kineticaw)==0:
-                        print "ERROR: can't find any kinetic parameters for reaction", reaction_name
+                        print ("ERROR: can't find any kinetic parameters for reaction", reaction_name)
                         exit(-3)
-                    elif react.getReversible(): 
+                    elif react.getReversible():
                         #print "WARNING: detected reversible reaction by getReversible", reaction_name
-                        create_reverse = True                    
+                        create_reverse = True
                     elif len(parameters_in_kineticaw)==2:
-                        print "WARNING: detected two parameters in kinetic law of reaction", reaction_name, ", assuming reversible reaction"
-                        create_reverse = True                    
+                        print ("WARNING: detected two parameters in kinetic law of reaction", reaction_name, ", assuming reversible reaction")
+                        create_reverse = True
                     elif len(parameters_in_kineticaw)==1:
                         pass
                     else:
-                        print "ERROR: too many parameters in kinetic law, aborting"
+                        print ("ERROR: too many parameters in kinetic law, aborting")
                         exit(-3)
 
-                    for el in parameters_in_kineticaw:                   
+                    for el in parameters_in_kineticaw:
 
                         p = self.model.getParameter(el)
 
-                        print len(parameters_in_kineticaw), p
-                        
+                        print (len(parameters_in_kineticaw), p)
+
                         if p.getValue()==0:
                             if not p.constant:
-                                print "WARNING: non constant parameter, assignment rule?"                            
+                                print ("WARNING: non constant parameter, assignment rule?")
                                 if self.model.getListOfRules().get(p.getName()).isParameter():
                                     # print " * Rule for parameter", p.getName(), "detected"
                                     # print " * Rule implemented as", self.model.getListOfRules().get("k1").getFormula()
@@ -324,20 +324,20 @@ class SBMLloader(object):
                                     tokenized_rule = tokenized_rule.replace(",", "")
                                     tokenized_rule =  tokenized_rule.split()
                                     temp = 0
-                                    for token in tokenized_rule:                                       
+                                    for token in tokenized_rule:
                                         try:
                                             temp = float(token)
                                             if temp>0:
                                                 break
-                                        except: 
+                                        except:
                                             pass
                                             #print token, "is NAN"
 
-                                    self.PARAMS.append(temp)                
+                                    self.PARAMS.append(temp)
                                     #print p.getName(), float(token)
                             else:
                                 #print "WARNING: constant value set to 0, parameter:", p.getName()
-                                self.PARAMS.append(temp)           
+                                self.PARAMS.append(temp)
                         else:
                             self.PARAMS.append(p.getValue())
                 else:
@@ -345,7 +345,7 @@ class SBMLloader(object):
                         self.PARAMS.append(p.getValue())
 
             # append reaction name
-            self.reaction_names.append(reaction_name)                
+            self.reaction_names.append(reaction_name)
             if create_reverse:
                 self.reaction_names.append(reaction_name+" (reverse)")
 
@@ -353,9 +353,9 @@ class SBMLloader(object):
             temp_reactants = []
             for reactant in react.getListOfReactants():
                 stoichiometry = reactant.getStoichiometry()
-                alias = self.id2name[reactant.getSpecies()]            
-                if alias == "": 
-                    alias = reactant.getSpecies()                
+                alias = self.id2name[reactant.getSpecies()]
+                if alias == "":
+                    alias = reactant.getSpecies()
                 index = self.species_dict[alias]
                 temp_reactants.append( str(stoichiometry) + "*" + alias )
                 reactants_vector[index]=stoichiometry
@@ -364,8 +364,8 @@ class SBMLloader(object):
             # append products
             temp_products = []
             for product in react.getListOfProducts():
-                alias = self.id2name[product.getSpecies()]      
-                if alias == "": 
+                alias = self.id2name[product.getSpecies()]
+                if alias == "":
                     alias = reactant.getSpecies()
                 stoichiometry = product.getStoichiometry()
                 index = self.species_dict[alias]
@@ -389,25 +389,24 @@ class SBMLloader(object):
 if __name__ == '__main__':
 
     INPUT_FILE = "Breast_core.xml"
-    OUTPUT_FOLDER = "./output_marzia"
+    OUTPUT_FOLDER = "./test_conv"
 
     if len(sys.argv)>1: INPUT_FILE = sys.argv[1]
     if len(sys.argv)>2: INPUT_FILE = sys.argv[2]
 
-    SL = SBMLloader(INPUT_FILE)  
+    SL = SBMLloader(INPUT_FILE)
     SL.convert_to_biosimware(OUTPUT_FOLDER)
-    print "Reaction names", SL.reaction_names
+    print ("Reaction names", SL.reaction_names)
     separator()
-    print "Reaction parameters", SL.PARAMS
+    print ("Reaction parameters", SL.PARAMS)
     separator()
-    print "PSA kinetic parameters", map(lambda x: "K_"+x, SL.reaction_names)
+    print ("PSA kinetic parameters", map(lambda x: "K_"+x, SL.reaction_names))
     separator()
-    print "PSA chemical species", map(lambda x: x[0], SL.sorted_species_dict)
+    print ("PSA chemical species", map(lambda x: x[0], SL.sorted_species_dict))
     separator()
-    print "FBA boundaries", SL.fluxes_boundaries
+    print ("FBA boundaries", SL.fluxes_boundaries)
     separator()
-    print "Reactants", SL.reactants
+    print ("Reactants", SL.reactants)
     separator()
-    print "Products", SL.products
+    print ("Products", SL.products)
     separator()
-    
