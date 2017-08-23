77d3b7e1110fa5b469cb1ac07180e1518e1b3aff
Ilario Tagliaferri <ilario.tagliaferri@gmail.com>
2017-08-23 17:09:16 +0200

    Production of output folder. Production of all output but boudiaries file

--- SBML2BSW_oo.py
+++ SBML2BSW_oo.py
@@ -102,11 +102,24 @@ class reaction(SBML_Object):
 #===== END =====    
 
 #==== Function definitions ====
-#For now functions, for testing purpose
-def react(model):
+def create_folder(OUTPATH):
+    """
+    Creates the specified output folder
+    """
+    try: os.mkdir(OUTPATH)
+    except: print ("WARNING: directory", OUTPATH, "already exists")
 
+#For now functions, for testing purpose
+def react(model,OUTPATH):
+
+    #----Lists needed for the output---
     LEFT = []
     RIGHT = []
+    ALPHABET = []
+    C_VECT = []
+    IN_AMOUNT = []
+    M_FEED = []
+    #---- end ----
     
     #list of all species for now, a species object is created for each
     #object in the model. With models with a lot of reactants it's not
@@ -114,37 +127,51 @@ def react(model):
     
     Species_list=[]
     Species_ID=[]
+    
+    
     for chem in model.getListOfSpecies():
         a=species(chem)
         Species_list.append(a)
         Species_ID.append(a.ID)
 
+        #prepare alphabet
+        Alph_element = a.name+"_in_"+a.compartment
+        ALPHABET.append(Alph_element)
+
+        #prepare initial amount
+        IN_AMOUNT.append(a.conc)
+
+        #prepare M_feed
+        if a.spec_con:
+            M_FEED.append(1)
+            print("1")
+        else:
+            M_FEED.append(0)
+            print("0")
+    print (M_FEED)
+
+    for par in model.getListOfParameters():
+        Parameter=parameter(par)
+#        print (Parameter.value)
+        C_VECT.append(Parameter.value)
+
+    
     reactants_vetctor=[]
     #Maniputlating the reactants of each reaction
-    for i in model.getListOfReactions():
+    for j in model.getListOfReactions():
         tmp_reactants = [0] * len(Species_list)
-        rc = reaction(i)
+        rc = reaction(j)        
         for i in rc.react_list:
             alias = i.getSpecies()
             index = Species_ID.index(alias)
             if alias in Species_ID:
                 spe  = Species_list[index]
-                fullname = spe.name
-                if fullname == "":
-                    fullname = spe.D
-                if spe.compartment != "":
-                    fullname = fullname + "_in_"+spe.compartment
                 sto = i.getStoichiometry()
                 tmp_reactants[index] = int(sto)
             else:
                 tmp_reactants[index] = 0
-                    
-            print (tmp_reactants)
 
-            LEFT.append(tmp_reactants)
-    print (LEFT)
-    
-    numpy.savetxt("left_side", LEFT, fmt="%d", delimiter="\t") 
+        LEFT.append(tmp_reactants)
     
     products_vetctor=[]
     #Maniputlating the products of each reaction
@@ -157,11 +184,7 @@ def react(model):
                 index = Species_ID.index(alias)
                 if alias in Species_ID:
                     spe  = Species_list[index]
-                    fullname = spe.name
-                    if fullname == "":
-                        fullname = spe.D
                     if spe.compartment != "":
-                        fullname = fullname + "_in_"+spe.compartment
                         sto = i.getStoichiometry()
                         tmp_products[index] = int(sto)
                     else:
@@ -169,12 +192,19 @@ def react(model):
                 else:
                     for index in len(Species_ID)-1:
                         tmp_products[index] = 0
-                    
-            print (tmp_products)
 
             RIGHT.append(tmp_products)
-    print (RIGHT)
+            
+    os.chdir(OUTPUT_FOLDER)
+    numpy.savetxt("alphabet", ALPHABET,fmt="%s", delimiter="\t",newline="\t")
+    numpy.savetxt("M_0",IN_AMOUNT,fmt="%e", delimiter="\t",newline="\t")
+    numpy.savetxt("M_feed",M_FEED,fmt="%d", delimiter="\t",newline="\t")
+    numpy.savetxt("c_vector",C_VECT,fmt="%e",delimiter="\t")
+    numpy.savetxt("left_side", LEFT, fmt="%d", delimiter="\t") 
     numpy.savetxt("right_side", RIGHT, fmt="%d", delimiter="\t") 
 #==== END ====
 
-react(model)
+OUTPUT_FOLDER = "./output"
+
+create_folder(OUTPUT_FOLDER)
+react(model,OUTPUT_FOLDER)
