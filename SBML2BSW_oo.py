95b0c42eb7365b22ba5436dd20fcfda03c5b1db5
Ilario Tagliaferri <ilario.tagliaferri@gmail.com>
2017-08-29 14:56:21 +0200

    Created main class.
    No kinetic rule or reverse up to now.
    
    Print to be fixed.

--- SBML2BSW_oo.py
+++ SBML2BSW_oo.py
@@ -15,17 +15,6 @@ It's written in python 3.
 THIS VERSION IS UNDER DEVELOPEMENT!
 """
 
-#FIX the REACTACT MATRICE
-
-#===This part is to conduct the test===
-#create a model object
-
-model = None
-#sbml = libsbml.SBMLReader().readSBML("Dismutase.xml")
-#model = sbml.getModel()
-#=== END ===
-
-
 #===== Constructors Definitions =====
 class SBML_Object(object):
     """
@@ -99,185 +88,231 @@ class reaction(SBML_Object):
         self.react_list = rt.getListOfReactants()
         self.prod_list = rt.getListOfProducts()
         self.rev = rt.getReversible()
-        
+
 #===== END =====    
 
 #==== Function definitions ====
-def create_folder(OUTPATH):
-    """
-    Creates the specified output folder
-    """
-    wd = os.path.dirname(__file__)
-    try: os.mkdir(OUTPATH)
-    except: print ("WARNING: directory", OUTPATH, "already exists")
+# def create_folder(OUTPATH):
+#     """
+#     Creates the specified output folder
+#     """
+#     wd = os.path.dirname(__file__)
+#     try: os.mkdir(OUTPATH)
+#     except: print ("WARNING: directory", OUTPATH, "already exists")
 
-#For now functions, for testing purpose
-def react(model,OUTPATH):
+def separator():
+    print ("")
+    print ("="*100)
+    print ("")
+#==== END ====
 
-    #----Lists needed for the output---
-    LEFT = []
-    RIGHT = []
-    ALPHABET = []
-    C_VECT = []
-    IN_AMOUNT = []
-    M_FEED = []
-    PARAMS = []
-    #---- end ----
-    
-    #list of all species for now, a species object is created for each
-    #object in the model. With models with a lot of reactants it's not
-    #good.
-    
-    Species_list=[]
-    Species_ID=[]
-    
-    for chem in model.getListOfSpecies():
-        a=species(chem)
-        Species_list.append(a)
-        Species_ID.append(a.ID)
-        
-        #--prepare alphabet
-        if a.name != "":
-            Alph_element = a.name+"_in_"+a.compartment
-        else:
-            Alph_element = a.ID+"_in_"+a.compartment
+#==== PRINCIPAL CLASS ====
 
-        ALPHABET.append(Alph_element)
+class SBML2BSW():
+    def __init__(self,model,OUTPATH):
+        self.model=model
+        self.out=OUTPATH
 
-        #--prepare initial amount
 
-        amount=0
-        if isnan(a.amount):
-            print("isnan")
-            amount=float(a.conc)
-            print(amount)
-        else:
-            print("is a num")
-            amount=float(a.amount)
-            print(amount)
+    wd=os.path.dirname(os.path.abspath(__file__))
+
+    def create_folder(self):
+        """
+        Creates the specified output folder
+        """
+        try: os.mkdir(self.out)
+        except: print ("WARNING: directory", self.out, "already exists")
             
-        IN_AMOUNT.append(amount)
+        
+    def react(self,model):
 
-        #--prepare M_feed
-        if a.spec_con:            
-            M_FEED.append(a.spec_con)
-        else:
-            M_FEED.append(a.spec_con)
+        #list needed
+        self.ALPHABET = []
+        self.Species_list = []
+        self.Species_ID = []
+        self.IN_AMOUNT = []
+        self.M_FEED = []
+        self.LEFT = []
+        self.RIGHT = []
+        self.PARAMS = []
 
-    #----Maniputlating the reactants of each reaction
-    for j in model.getListOfReactions():
-        tmp_reactants = numpy.zeros(len(Species_list))
-        rc = reaction(j)
-        for i in rc.react_list:
-            alias = i.getSpecies()
-            index = Species_ID.index(alias)
-            if alias in Species_ID:
-                spe  = Species_list[index]
-                sto = i.getStoichiometry()
-                tmp_reactants[index] = int(sto)
+        self.REACT_NAME = []
+        
+        for chem in model.getListOfSpecies():
+            a=species(chem)
+            self.Species_list.append(a)
+            self.Species_ID.append(a.ID)
+            #--prepare alphabet
+            if a.name != "":
+                Alph_element = a.name+"_in_"+a.compartment
             else:
-                tmp_reactants[index] = 0
+                Alph_element = a.ID+"_in_"+a.compartment
 
-        LEFT.append(tmp_reactants)
-    
-    #----Maniputlating the products of each reaction
-    
-        tmp_products =  numpy.zeros(len(Species_list))
-        if rc.prod_list != "":
-            for i in rc.prod_list:
-                alias = i.getSpecies()
-                index = Species_ID.index(alias)
-                if alias in Species_ID:
-                    spe  = Species_list[index]
-                    if spe.compartment != "":
-                        sto = i.getStoichiometry()
-                        tmp_products[index] = int(sto)
+            self.ALPHABET.append(Alph_element)
+
+            #-- prepare initial amount
+            
+            
+            amount=0
+            if isnan(a.amount):
+                amount=float(a.conc)
+            else:
+                amount=float(a.amount)
+
+            self.IN_AMOUNT.append(amount)
+                
+            #--prepare M_feed
+            
+            if a.spec_con:
+                self.M_FEED.append(a.spec_con)
+
+        #----Maniputlating the reactants of each reaction
+        
+        for react in model.getListOfReactions():
+            
+            tmp_reactants = numpy.zeros(len(self.Species_list))
+            tmp_products = numpy.zeros(len(self.Species_list))
+            
+            rc = reaction(react)
+            for single_react in rc.react_list:
+                react_species = single_react.getSpecies()
+                index = self.Species_ID.index(react_species)
+                if react_species in self.Species_ID:
+                    spe  = self.Species_list[index]
+                    sto = single_react.getStoichiometry()
+                    tmp_reactants[index] = int(sto)
+                # else:
+                #     tmp_reactants[index] = 0
+                        
+            self.LEFT.append(tmp_reactants)
+
+            #----Maniputlating the reactants of each reactio
+            for prods in rc.prod_list:
+                prod_species = prods.getSpecies()
+                index = self.Species_ID.index(prod_species)
+                if prod_species in self.Species_ID:
+                    spe = self.Species_list[index]
+                    sto = prods.getStoichiometry()
+                    tmp_products[index] = int(sto)
+            self.RIGHT.append(tmp_products)
+
+            #---- Create the constant vector
+            if rc.kin_law != None:
+                if len(rc.kin_law.getListOfParameters())==0:
+                    nomi_parametri = [par.getId() for par in self.model.getListOfParameters() ]
+                    parameters_in_kineticaw = re.findall(r"[\w']+", rc.kin_law.getFormula())
+                    parameters_in_kineticaw = [x.strip() for x in parameters_in_kineticaw ]
+                    #filter object in python3 don't have len attribute, converting to string
+                    parameters_in_kineticaw = list(filter( lambda x: x in nomi_parametri, parameters_in_kineticaw))
+                    print(parameters_in_kineticaw)
+                    if len(parameters_in_kineticaw)==0:
+                        print ("parameters_in_kineticaw==0")
+                        print ("ERROR: can't find any kinetic parameters for reaction", rc.name)
+                        exit(-3)
+                    elif rc.rev:
+                        #print "WARNING: detected reversible reaction by getReversible", reaction_name
+                        create_reverse = True
+                    elif len(list(parameters_in_kineticaw))==2:
+                        print ("WARNING: detected two parameters in kinetic law of reaction", rc.name, ", assuming reversible reaction")
+                        create_reverse = True
+                    elif len(list(parameters_in_kineticaw))==1:
+                        pass
                     else:
-                        tmp_products[index] = 0
-                else:
-                    for index in len(Species_ID)-1:
-                        tmp_products[index] = 0
+                        print ("ERROR: too many parameters in kinetic law, aborting")
+                        exit(-3)
+                    
+                    for el in parameters_in_kineticaw:                   
+                        p = self.model.getParameter(el)
+                        print (len(parameters_in_kineticaw), p)
+                        
+                        if p.getValue()==0:
+                            if not p.constant:
+                               print ("WARNING: non constant parameter, assignment rule?")
+                               if model.getListOfRules().get(p.getName()).isParameter():
+                                   # print " * Rule for parameter", p.getName(), "detected"
+                                   # print " * Rule implemented as", self.model.getListOfRules().get("k1").getFormula()
+                                   tokenized_rule = model.getListOfRules().get("k1").getFormula()
+                                   if tokenized_rule[0:8] == 'stepfunc':
+                                       tokenized_rule = tokenized_rule.replace("stepfunc(", "")
+                                       tokenized_rule = tokenized_rule.replace(")", "")
+                                       tokenized_rule = tokenized_rule.replace(",", "")
+                                       tokenized_rule =  tokenized_rule.split()
+                                       temp = 0
+                                   for token in tokenized_rule:                                       
+                                       try:
+                                           temp = float(token)
+                                           if temp>0:
+                                               break
+                                       except: 
+                                           pass
+                                       #print token, "is NAN"
 
-            RIGHT.append(tmp_products)
-
-        if rc.kin_law != None:
-            if len(rc.kin_law.getListOfParameters())==0:
-                nomi_parametri = [par.getId() for par in model.getListOfParameters() ]
-                parameters_in_kineticaw = re.findall(r"[\w']+", rc.kin_law.getFormula())
-                parameters_in_kineticaw = [x.strip() for x in parameters_in_kineticaw ]
-                #filter object in python3 don't have len attribute, converting to string
-                parameters_in_kineticaw = list(filter( lambda x: x in nomi_parametri, parameters_in_kineticaw))
-                print(parameters_in_kineticaw)
-                if len(parameters_in_kineticaw)==0:
-                    print ("parameters_in_kineticaw==0")
-                    print ("ERROR: can't find any kinetic parameters for reaction", rc.name)
-                    exit(-3)
-                elif rc.rev:
-                    #print "WARNING: detected reversible reaction by getReversible", reaction_name
-                    create_reverse = True
-                elif len(list(parameters_in_kineticaw))==2:
-                    print ("WARNING: detected two parameters in kinetic law of reaction", rc.name, ", assuming reversible reaction")
-                    create_reverse = True
-                elif len(list(parameters_in_kineticaw))==1:
-                    pass
-                else:
-                    print ("ERROR: too many parameters in kinetic law, aborting")
-                    exit(-3)
-                    
-                for el in parameters_in_kineticaw:                   
-                    p = model.getParameter(el)
-                    
-                    print (len(parameters_in_kineticaw), p)
-                    
-                    if p.getValue()==0:
-                        if not p.constant:
-                            print ("WARNING: non constant parameter, assignment rule?")
-                            if model.getListOfRules().get(p.getName()).isParameter():
-                                # print " * Rule for parameter", p.getName(), "detected"
-                                # print " * Rule implemented as", self.model.getListOfRules().get("k1").getFormula()
-                                tokenized_rule = model.getListOfRules().get("k1").getFormula()
-                                if tokenized_rule[0:8] == 'stepfunc':
-                                    tokenized_rule = tokenized_rule.replace("stepfunc(", "")
-                                    tokenized_rule = tokenized_rule.replace(")", "")
-                                    tokenized_rule = tokenized_rule.replace(",", "")
-                                    tokenized_rule =  tokenized_rule.split()
-                                    temp = 0
-                                for token in tokenized_rule:                                       
-                                    try:
-                                        temp = float(token)
-                                        if temp>0:
-                                            break
-                                    except: 
-                                        pass
-                                    #print token, "is NAN"
-                                    
-                                PARAMS.append(temp)                
-                                #print p.getName(), float(token)
+                                   self.PARAMS.append(temp)                
+                                   #print p.getName(), float(token)
                         else:
                             #print "WARNING: constant value set to 0, parameter:", p.getName()
-                            PARAMS.append(temp)           
+                            self.PARAMS.append(temp)           
+                    else:
+                        self.PARAMS.append(p.getValue())
                 else:
-                    PARAMS.append(p.getValue())
-            else:
-                for p in rc.kin_law.getListOfParameters():
-                    PARAMS.append(p.getValue())
-    
-    os.chdir(OUTPUT_FOLDER)
-    numpy.savetxt("c_vector", numpy.array(PARAMS),fmt="%e", delimiter="\t")
-    numpy.savetxt("alphabet", ALPHABET,fmt="%s", delimiter="\t",newline="\t")
-    numpy.savetxt("M_0",IN_AMOUNT,fmt="%e", delimiter="\t",newline="\t")
-    numpy.savetxt("M_feed",M_FEED,fmt="%d", delimiter="\t",newline="\t")
-    numpy.savetxt("left_side", LEFT, fmt="%d", delimiter="\t") 
-    numpy.savetxt("right_side", RIGHT, fmt="%d", delimiter="\t") 
+                    for p in rc.kin_law.getListOfParameters():
+                        self.PARAMS.append(p.getValue())
+
+
+    def save(self):
+        os.chdir(self.out)
+        numpy.savetxt("alphabet", self.ALPHABET,fmt="%s", delimiter="\t",newline="\t")
+        numpy.savetxt("M_0",self.IN_AMOUNT,fmt="%e", delimiter="\t",newline="\t")
+        numpy.savetxt("M_feed",self.M_FEED,fmt="%d", delimiter="\t",newline="\t")
+        numpy.savetxt("left_side", self.LEFT, fmt="%d", delimiter="\t")
+        numpy.savetxt("right_side", self.RIGHT, fmt="%d", delimiter="\t")
+        numpy.savetxt("c_vector", numpy.array(self.PARAMS),fmt="%e", delimiter="\t")
+        os.chdir(SBML2BSW.wd)
+
+#==== END ====
+
+
+
 #==== END ====
 
 if __name__ == '__main__':
+
+    AA = None
+
     OUTPUT_FOLDER = "./output"
 
     if len(sys.argv)>1: INPUT_FILE = sys.argv[1]
 
     sbml = libsbml.SBMLReader().readSBML(INPUT_FILE)
-    model = sbml.getModel()
+    AA = sbml.getModel()
 
-    create_folder(OUTPUT_FOLDER)
-    react(model,OUTPUT_FOLDER)
+    SB=SBML2BSW(AA,OUTPUT_FOLDER)
+    SB.create_folder()
+    SB.react(AA)
+    SB.save()
+    
+    separator()
+    
+    print("PSA chemical species",SB.ALPHABET)
+
+    separator()
+
+    print("Chemicals Initial Amount",SB.IN_AMOUNT)
+
+    separator()
+
+    print(SB.M_FEED)
+
+    separator()
+
+    print("Reactants:",SB.LEFT)
+
+    separator()
+
+    print("Products:",SB.RIGHT)
+
+    separator()
+
+    print(SB.PARAMS)
+
+    separator()
